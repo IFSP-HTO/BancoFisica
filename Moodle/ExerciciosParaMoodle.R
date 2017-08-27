876b009b9b4bd6cc870d144b60de54cc1deabfbf
vinibrenobr11 <vinibrenobr11@hotmail.com>
2017-08-26 22:07:31 -0300

    Q07CalS & Erros corrigidos em /Moodle

--- Moodle/ExerciciosParaMoodle.R
+++ Moodle/ExerciciosParaMoodle.R
@@ -2,59 +2,71 @@
 
 ## load package
 library(tools)
-library("exams")
+library(exams)
 
 ############### velocidade media ##################
 ## Definindo a pasta com as questoes do exame
-myexam <- dir("../BancoDeQuestoes/vm", pattern = ".rnw", ignore.case=TRUE)
+myexam <- dir("./BancoDeQuestoes/vm", pattern = ".rnw", ignore.case=TRUE)
 ano <- 12017
 ## Cria o arquivo .xml para entrada no moodle
 set.seed(ano)
-exams2moodle(myexam, n = 100, rule="none", schoice = list(shuffle = TRUE), name = paste0("vm-",ano),
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("vm-",ano),
              encoding = "UTF-8",
-             dir = "../Moodle",
-             edir = "../BancoDeQuestoes/vm")
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/vm")
 
 ############### exemplos ##################
 ## Definindo a pasta com as questoes do exame
-myexam <- dir("../BancoDeQuestoes/exemplos", pattern = ".rnw", ignore.case=TRUE)
+myexam <- dir("./BancoDeQuestoes/exemplos", pattern = ".rnw", ignore.case=TRUE)
 ano <- 12017
 ## Cria o arquivo .xml para entrada no moodle
 set.seed(ano)
-exams2moodle(myexam, n = 5, rule="none", schoice = list(shuffle = TRUE), name = paste0("exemplos-",ano),
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("exemplos-",ano),
              encoding = "UTF-8",
-             dir = "../Moodle",
-             edir = "../BancoDeQuestoes/exemplos")
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/exemplos")
 
 ############### dilatacao #################
 ## Definindo a pasta com as questoes do exame
-myexam <- dir("../BancoDeQuestoes/dilatterm", pattern = ".rnw", ignore.case=TRUE)
+myexam <- dir("./BancoDeQuestoes/dilatterm", pattern = ".rnw", ignore.case=TRUE)
 ano <- 12017
 ## Cria o arquivo .xml para entrada no moodle
 set.seed(ano)
-exams2moodle(myexam, n = 100, rule="none", schoice = list(shuffle = TRUE), name = paste0("dilatterm-",ano),
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("dilatterm-",ano),
              encoding = "UTF-8",
-             dir = "../Moodle",
-             edir = "../BancoDeQuestoes/dilatterm")
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/dilatterm")
 
 ############### calortemp ##################
 ## Definindo a pasta com as questoes do exame
-myexam <- dir("../BancoDeQuestoes/calortemp", pattern = ".rnw", ignore.case=TRUE)
+myexam <- dir("./BancoDeQuestoes/calortemp", pattern = ".rnw", ignore.case=TRUE)
 ano <- 12017
 ## Cria o arquivo .xml para entrada no moodle
 set.seed(ano)
-exams2moodle(myexam, n = 100, rule="none", schoice = list(shuffle = TRUE), name = paste0("calortemp-",ano),
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("calortemp-",ano),
              encoding = "UTF-8",
-             dir = "../Moodle",
-             edir = "../BancoDeQuestoes/calortemp")
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/calortemp")
+
+############### calorimetria ###############
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/calorimetria", pattern = ".rnw", ignore.case=TRUE)
+ano <- 12017
+## Cria o arquivo .xml para entrada no moodle
+set.seed(ano)
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("calorimetria-",ano),
+             encoding = "UTF-8",
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/calorimetria")
+
 
 ############### aceleracao ##################
 ## Definindo a pasta com as questoes do exame
-myexam <- dir("../BancoDeQuestoes/acel", pattern = ".rnw", ignore.case=TRUE)
+myexam <- dir("./BancoDeQuestoes/acel", pattern = ".rnw", ignore.case=TRUE)
 ano <- 12017
 ## Cria o arquivo .xml para entrada no moodle
 set.seed(ano)
-exams2moodle(myexam, n = 100, rule="none", schoice = list(shuffle = TRUE), name = paste0("acel-",ano),
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("acel-",ano),
              encoding = "UTF-8",
-             dir = "../Moodle",
-             edir = "../BancoDeQuestoes/acel")
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/acel")
