0cdea6672b0b94ec33b2ac09807f4d9a7fbb7364
flavio <flaviomargarito@gmail.com>
2019-05-20 12:03:01 -0300

    Resolvendo conflito.

--- R/render.R
+++ R/render.R
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 rmarkdown::render(input = "doc/README.Rmd", 
-                  output_format = "github_document",
-=======
-rmarkdown::render(input = "doc/README.Rmd",
->>>>>>> correct_docs
-                  output_file = 'README.md')
+                  output_format = "github_document")
+
